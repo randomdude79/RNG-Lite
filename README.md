cedd55f896c227860efe7242c63482accbcb5ffe
randomdude79 <158773778+randomdude79@users.noreply.github.com>
2025-07-28 13:00:14 +0000

    v1.0.0

---
+++ README.md
@@ -0,0 +1,41 @@
+# Random Number Generator
+![logo](/noncode/logo.png)
+
+This Python program generates random numbers based on user input. It allows users to specify a range of numbers and the number of random numbers to generate. The program also supports the use of a seed for deterministic random number generation.
+
+## Features
+
+- Generates random numbers within a specified range.
+- Supports the use of a seed for deterministic results.
+- Handles user input validation to ensure that only integers are accepted.
+- Detects and filters profanity in user email and feedback inputs.
+
+## Getting Started
+
+1. Clone the repository to your local machine.
+3. Run the program using `python main.py`.
+4. Follow the on-screen instructions to generate random numbers.
+
+## Usage
+
+- Upon running the program, you will be prompted to enter a username.
+- You will then be asked if you want to use a seed for random number generation.
+- Next, you will be prompted to choose a range of numbers (minimum and maximum) and the number of random numbers to generate.
+- The program will generate the random numbers and display them on the screen.
+- You can also choose to find the average of the generated numbers and copy the seed used for generation.
+
+## Feedback
+
+- Users can provide feedback on the program by entering their feedback and email address.
+- The program detects and filters profanity in both the feedback and email inputs.
+
+## Contributions
+
+- Contributions to the program are welcome. Please fork the repository, make your changes, and submit a pull request.
+
+## License
+
+- This project is licensed under the Apache 2.0 License. See the [`LICENSE`](LICENSE) file for details.
+
+## Author's Notes
+- If anybody wants to help with this project, contact me through email [here.](https://mail.google.com/mail/u/0/?fs=1&to=sashapbaranov@gmail.com&tf=cm)
---
+++ main.py
@@ -0,0 +1,98 @@
+import statistics, random, time, os, sys
+
+version = "1.12.3"
+seed = []
+numbersList = []
+
+def copy(seed):
+    with open('seed.txt', 'w') as s:
+        s.write(str(seed))
+        print('Check seed.txt for your seed')
+
+def clear():
+    os.system('clear')
+
+def exit_program():
+    sys.exit()
+
+def is_an_integer(s):
+    try: int(s); return True
+    except ValueError: return False
+
+def get_int(prompt):
+    while True:
+        i = input(prompt)
+        if i == "-0":
+            print('-0 is not an integer. Please try again.\n')
+            continue
+        if i == "clear":
+            clear()
+            print('Console cleared!\n')
+            random_gen()
+        elif i == "updates":
+            with open('noncode/UpdateLog.txt', 'r') as f:
+                print(f.read())
+            print(f'Current version: {version}')
+            print('For more, check out https://github.com/randomdude79/Random-Number-Generator')
+            random_gen()
+        elif i == "update":
+            clear()
+            for _ in range(3):
+                for dots in ["   ", ".  ", ".. ", "..."]:
+                    sys.stdout.write(f'Updating{dots}\r')
+                    time.sleep(0.5)
+            os.system('git restore * && git pull')
+            for _ in range(3):
+                for dots in ["   ", ".  ", ".. ", "..."]:
+                    sys.stdout.write(f'Restarting{dots}\r')
+                    time.sleep(0.5)
+            clear()
+            print(f'Version: {version}')
+            os.system('python main.py')
+        elif i == "exit":
+            exit_program()
+        elif is_an_integer(i):
+            return int(i)
+        else:
+            print(f'"{i}" is not an integer. Please enter an integer.\n')
+
+def random_gen():
+    global seed, numbersList
+    numbersList = []
+    randomInt1 = None
+    if input('Would you like to use a seed?\n').lower() == 'yes':
+        seed = int(input('Enter a seed:\n'))
+    else:
+        seed = int(''.join(str(random.randint(0, 9)) for _ in range(20)))
+    random.seed(seed)
+
+    number1 = get_int('Choose your first number.\n')
+    print()
+    number2 = get_int('Choose your second number.\n')
+    print()
+    amount = get_int('How many numbers do you want to generate?\n')
+    print()
+
+    number1, number2 = max(number1, number2), min(number1, number2)
+
+    if amount > 1:
+        print('Your random numbers are:')
+        for _ in range(amount):
+            num = random.randint(number2, number1)
+            print(num)
+            numbersList.append(num)
+        print()
+        if input('Would you like to find the average of all your numbers?\n').lower() == 'yes':
+            print('\nThe average of all your numbers is:', statistics.mean(numbersList), '\n')
+        if input('Would you like to know your seed?\n').lower() == 'yes':
+            print('Your seed is:', seed)
+            if input('Would you like to copy the seed?\n').lower() == 'yes':
+                copy(seed)
+        print()
+    else:
+        print('Your random number is:\n', random.randint(number2, number1))
+    random.seed()
+    print()
+
+while True:
+    random_gen()
