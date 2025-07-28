b62c58c4c9269b6442b1d186346b40f4bd88d745
randomdude79 <158773778+randomdude79@users.noreply.github.com>
2025-07-28 18:50:54 +0000

    v1.0.4
    Minor fixes

--- main.py
+++ main.py
@@ -49,6 +49,9 @@ def get_int(prompt):
             loading_bar(duration=10, bar_length=50)
             os.system('git restore * && git pull')
             clear()
+            print('Done with downloading updates!')
+            for i in range(3):
+                print(f'Restarting in {3-i} seconds...')
             for _ in range(3):
                 for dots in ["   ", ".  ", ".. ", "..."]:
                     sys.stdout.write(f'Restarting{dots}\r')
